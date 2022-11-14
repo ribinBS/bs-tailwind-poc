59d58013bf64252068c10af7a6ff5ff3062b2f46
ribinBS <ribin@browserstack.com>
2022-11-14 21:27:25 +0530

    basic tw init done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from 'react';
+import SideBar from './components/SideBar';
+
 function App() {
-    return <div className='App'>T</div>;
+    return (
+        <div className='App'>
+            <SideBar />
+        </div>
+    );
 }
 
 export default App;
