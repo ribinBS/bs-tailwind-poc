65475466135a31e7dc03b9d2b5e648f9cbe6887c
ribinBS <ribin@browserstack.com>
2022-11-16 13:55:46 +0530

    width updated

--- src/pages/Details/MainSection.jsx
+++ src/pages/Details/MainSection.jsx
@@ -423,7 +423,7 @@ export default function MainSection() {
                                     </Menu.Items>
                                 </Transition>
                             </Menu>
-                            <div className='relative  rounded-md shadow-sm'>
+                            <div className='relative  rounded-md shadow-sm w-64'>
                                 <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3'>
                                     <EnvelopeIcon
                                         className='h-5 w-5 text-gray-400'
