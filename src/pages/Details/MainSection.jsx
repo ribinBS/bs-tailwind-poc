a7f8442926f7f245fb6a4c74420aaa0d998c84f3
ribinBS <ribin@browserstack.com>
2022-11-16 10:39:09 +0530

    changes done

--- src/pages/Details/MainSection.jsx
+++ src/pages/Details/MainSection.jsx
@@ -56,6 +56,157 @@ export default function MainSection() {
                     </span>
                 </div>
             </div>
+            <div className='flex'>
+                <a href='#' className='mr-7 group block flex-shrink-0'>
+                    <div className='flex items-center'>
+                        <div>
+                            <img
+                                className='inline-block h-9 w-9 rounded-full'
+                                src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                                alt=''
+                            />
+                        </div>
+                        <div className='ml-3'>
+                            <p className='text-sm font-medium text-gray-700 group-hover:text-gray-900'>
+                                3342895
+                            </p>
+                        </div>
+                    </div>
+                </a>
+                <a href='#' className='mr-7 group block flex-shrink-0'>
+                    <div className='flex items-center'>
+                        <div>
+                            <img
+                                className='inline-block h-9 w-9 rounded-full'
+                                src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                                alt=''
+                            />
+                        </div>
+                        <div className='ml-3'>
+                            <p className='text-sm font-medium text-gray-700 group-hover:text-gray-900'>
+                                Firefox
+                            </p>
+                            <p className='text-xs font-medium text-gray-500 group-hover:text-gray-700'>
+                                <b>Version:</b> 105.0
+                            </p>
+                        </div>
+                    </div>
+                </a>
+                <a href='#' className='group block flex-shrink-0'>
+                    <div className='flex items-center'>
+                        <div>
+                            <img
+                                className='inline-block h-9 w-9 rounded-full'
+                                src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                                alt=''
+                            />
+                        </div>
+                        <div className='ml-3'>
+                            <p className='text-sm font-medium text-gray-700 group-hover:text-gray-900'>
+                                Windows
+                            </p>
+                            <p className='text-xs font-medium text-gray-500 group-hover:text-gray-700'>
+                                <b>Version:</b> 8.1
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div className='flex mt-6 flex-wrap'>
+                <h4>Tags</h4>
+                <div className='flex mt-2 flex-wrap'>
+                    <div className='inline-flex rounded-md shadow-sm mr-2 mb-2'>
+                        <div className='relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 '>
+                            DOMException.code
+                        </div>
+                        <div as='div' className='relative -ml-px block'>
+                            <div className='relative inline-flex items-center rounded-r-md border border-gray-300 bg-gray-50 px-2 py-1.5 text-xs font-medium text-blue-500 '>
+                                11
+                            </div>
+                        </div>
+                    </div>
+                    <div className='inline-flex rounded-md shadow-sm mr-2 mb-2'>
+                        <div className='relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 '>
+                            browser
+                        </div>
+                        <div as='div' className='relative -ml-px block'>
+                            <div className='relative inline-flex items-center rounded-r-md border border-gray-300 bg-gray-50 px-2 py-1.5 text-xs font-medium text-blue-500 '>
+                                11
+                            </div>
+                        </div>
+                    </div>
+                    <div className='inline-flex rounded-md shadow-sm mr-2 mb-2'>
+                        <div className='relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 '>
+                            browser.name
+                        </div>
+                        <div as='div' className='relative -ml-px block'>
+                            <div className='relative inline-flex items-center rounded-r-md border border-gray-300 bg-gray-50 px-2 py-1.5 text-xs font-medium text-blue-500 '>
+                                11
+                            </div>
+                        </div>
+                    </div>
+                    <div className='inline-flex rounded-md shadow-sm mr-2 mb-2'>
+                        <div className='relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 '>
+                            environment
+                        </div>
+                        <div as='div' className='relative -ml-px block'>
+                            <div className='relative inline-flex items-center rounded-r-md border border-gray-300 bg-gray-50 px-2 py-1.5 text-xs font-medium text-blue-500 '>
+                                11
+                            </div>
+                        </div>
+                    </div>
+                    <div className='inline-flex rounded-md shadow-sm mr-2 mb-2'>
+                        <div className='relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 '>
+                            browser
+                        </div>
+                        <div as='div' className='relative -ml-px block'>
+                            <div className='relative inline-flex items-center rounded-r-md border border-gray-300 bg-gray-50 px-2 py-1.5 text-xs font-medium text-blue-500 '>
+                                11
+                            </div>
+                        </div>
+                    </div>
+                    <div className='inline-flex rounded-md shadow-sm mr-2 mb-2'>
+                        <div className='relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 '>
+                            browser
+                        </div>
+                        <div as='div' className='relative -ml-px block'>
+                            <div className='relative inline-flex items-center rounded-r-md border border-gray-300 bg-gray-50 px-2 py-1.5 text-xs font-medium text-blue-500 '>
+                                11
+                            </div>
+                        </div>
+                    </div>
+                    <div className='inline-flex rounded-md shadow-sm mr-2 mb-2'>
+                        <div className='relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 '>
+                            browser
+                        </div>
+                        <div as='div' className='relative -ml-px block'>
+                            <div className='relative inline-flex items-center rounded-r-md border border-gray-300 bg-gray-50 px-2 py-1.5 text-xs font-medium text-blue-500 '>
+                                11
+                            </div>
+                        </div>
+                    </div>
+                    <div className='inline-flex rounded-md shadow-sm mr-2 mb-2'>
+                        <div className='relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 '>
+                            browser
+                        </div>
+                        <div as='div' className='relative -ml-px block'>
+                            <div className='relative inline-flex items-center rounded-r-md border border-gray-300 bg-gray-50 px-2 py-1.5 text-xs font-medium text-blue-500 '>
+                                11
+                            </div>
+                        </div>
+                    </div>
+                    <div className='inline-flex rounded-md shadow-sm mr-2 mb-2'>
+                        <div className='relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 '>
+                            browser
+                        </div>
+                        <div as='div' className='relative -ml-px block'>
+                            <div className='relative inline-flex items-center rounded-r-md border border-gray-300 bg-gray-50 px-2 py-1.5 text-xs font-medium text-blue-500 '>
+                                11
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
