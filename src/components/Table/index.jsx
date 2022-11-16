45e8114b08144d1fdbb27000c78ed2ccebb5bda6
arsalanPro <arsalan.k@browserstack.com>
2022-11-16 14:45:30 +0530

    table header changes

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,4 +1,13 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { MusicalNoteIcon, TrophyIcon } from "@heroicons/react/20/solid";
+import ButtonDropdown from "../ButtonDropdown";
+import EllipsisDropdown from "../EllipsisDropdown";
+import { Menu, Transition } from "@headlessui/react";
+import { ArrowDownIcon, ChevronDownIcon } from "@heroicons/react/20/solid";
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+}
 
 const people = [
     {
@@ -79,25 +88,6 @@ const people = [
 export default function Example() {
     return (
         <div className="px-4 sm:px-6 lg:px-8">
-            {/* <div className="sm:flex sm:items-center">
-                <div className="sm:flex-auto">
-                    <h1 className="text-xl font-semibold text-gray-900">
-                        Users
-                    </h1>
-                    <p className="mt-2 text-sm text-gray-700">
-                        A list of all the users in your account including their
-                        name, title, email and role.
-                    </p>
-                </div>
-                <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                    <button
-                        type="button"
-                        className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-                    >
-                        Add user
-                    </button>
-                </div>
-            </div> */}
             <div className="mt-8 flex flex-col">
                 <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
@@ -109,33 +99,170 @@ export default function Example() {
                                             scope="col"
                                             className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
                                         >
-                                            Name
+                                            <input
+                                                id="comments"
+                                                aria-describedby="comments-description"
+                                                name="comments"
+                                                type="checkbox"
+                                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                            />
+                                            <ButtonDropdown
+                                                title="Resolve"
+                                                icon={
+                                                    <TrophyIcon
+                                                        className="-ml-0.5 mr-2 h-4 w-4 text-gray-300"
+                                                        aria-hidden="true"
+                                                    />
+                                                }
+                                            />
+                                            <ButtonDropdown
+                                                title="Ignore"
+                                                icon={
+                                                    <MusicalNoteIcon
+                                                        className="-ml-0.5 mr-2 h-4 w-4 text-gray-300"
+                                                        aria-hidden="true"
+                                                    />
+                                                }
+                                            />
+                                            <EllipsisDropdown />
+                                            <Menu
+                                                as="div"
+                                                className="relative inline-block text-left ml-2"
+                                            >
+                                                <div>
+                                                    <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-2 py-1 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                                                        <ArrowDownIcon
+                                                            className="h-5 w-5"
+                                                            aria-hidden="true"
+                                                        />
+                                                        Last Seen
+                                                        <ChevronDownIcon
+                                                            className="-mr-1 ml-2 h-5 w-5"
+                                                            aria-hidden="true"
+                                                        />
+                                                    </Menu.Button>
+                                                </div>
+
+                                                <Transition
+                                                    as={Fragment}
+                                                    enter="transition ease-out duration-100"
+                                                    enterFrom="transform opacity-0 scale-95"
+                                                    enterTo="transform opacity-100 scale-100"
+                                                    leave="transition ease-in duration-75"
+                                                    leaveFrom="transform opacity-100 scale-100"
+                                                    leaveTo="transform opacity-0 scale-95"
+                                                >
+                                                    <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                        <div className="py-1">
+                                                            <Menu.Item>
+                                                                {({
+                                                                    active,
+                                                                }) => (
+                                                                    <a
+                                                                        href="#"
+                                                                        className={classNames(
+                                                                            active
+                                                                                ? "bg-gray-100 text-gray-900"
+                                                                                : "text-gray-700",
+                                                                            "block px-4 py-2 text-sm"
+                                                                        )}
+                                                                    >
+                                                                        Account
+                                                                        settings
+                                                                    </a>
+                                                                )}
+                                                            </Menu.Item>
+                                                            <Menu.Item>
+                                                                {({
+                                                                    active,
+                                                                }) => (
+                                                                    <a
+                                                                        href="#"
+                                                                        className={classNames(
+                                                                            active
+                                                                                ? "bg-gray-100 text-gray-900"
+                                                                                : "text-gray-700",
+                                                                            "block px-4 py-2 text-sm"
+                                                                        )}
+                                                                    >
+                                                                        Support
+                                                                    </a>
+                                                                )}
+                                                            </Menu.Item>
+                                                            <Menu.Item>
+                                                                {({
+                                                                    active,
+                                                                }) => (
+                                                                    <a
+                                                                        href="#"
+                                                                        className={classNames(
+                                                                            active
+                                                                                ? "bg-gray-100 text-gray-900"
+                                                                                : "text-gray-700",
+                                                                            "block px-4 py-2 text-sm"
+                                                                        )}
+                                                                    >
+                                                                        License
+                                                                    </a>
+                                                                )}
+                                                            </Menu.Item>
+                                                            <form
+                                                                method="POST"
+                                                                action="#"
+                                                            >
+                                                                <Menu.Item>
+                                                                    {({
+                                                                        active,
+                                                                    }) => (
+                                                                        <button
+                                                                            type="submit"
+                                                                            className={classNames(
+                                                                                active
+                                                                                    ? "bg-gray-100 text-gray-900"
+                                                                                    : "text-gray-700",
+                                                                                "block w-full px-4 py-2 text-left text-sm"
+                                                                            )}
+                                                                        >
+                                                                            Sign
+                                                                            out
+                                                                        </button>
+                                                                    )}
+                                                                </Menu.Item>
+                                                            </form>
+                                                        </div>
+                                                    </Menu.Items>
+                                                </Transition>
+                                            </Menu>
                                         </th>
                                         <th
                                             scope="col"
-                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                            className="flex justify-between px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                                         >
-                                            Title
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                                        >
-                                            Email
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                                        >
-                                            Role
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="relative py-3.5 pl-3 pr-4 sm:pr-6"
-                                        >
-                                            <span className="sr-only">
-                                                Edit
+                                            GRAPH:
+                                            <span className="font-normal">
+                                                24h
+                                                <span className="font-thin ml-2">
+                                                    14d
+                                                </span>
                                             </span>
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                        >
+                                            EVENTS
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                        >
+                                            USERS
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                        >
+                                            ASSIGNEE
                                         </th>
                                     </tr>
                                 </thead>
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -4,6 +4,10 @@ import Dropdowns from "../../components/Dropdowns";
 import Table from "../../components/Table";
 import Pagination from "../../components/Pagination";
 import Footer from "../../components/Footer";
+import {
+    EnvelopeIcon,
+    QuestionMarkCircleIcon,
+} from "@heroicons/react/20/solid";
 
 export default function Dashboard() {
     return (
@@ -27,8 +31,8 @@ export default function Dashboard() {
                 <div className="bg-gray-100 px-8">
                     <Tabs />
                 </div>
-                <div className="grid grid-flow-col auto-cols-auto my-6 px-6">
-                    <div className="flex">
+                <div className="flex my-6 px-6">
+                    <div className="flex flex-initial">
                         <div className="mx-2">
                             <Dropdowns name="My Projects" />
                         </div>
@@ -39,17 +43,39 @@ export default function Dashboard() {
                             <Dropdowns name="14D" />
                         </div>
                     </div>
-                    <div className="flex w-full">
+                    <div className="flex flex-1 mb-2">
                         <label htmlFor="email" className="sr-only">
                             Email
                         </label>
-                        <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                            placeholder="you@example.com"
-                        />
+                        <div className="relative mt-1 rounded-md shadow-sm w-full">
+                            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                                <EnvelopeIcon
+                                    className="h-5 w-5 text-gray-400"
+                                    aria-hidden="true"
+                                />
+                            </div>
+                            {/* <div className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 ">
+                                is:
+                            </div>
+                            <div as="div" className="relative -ml-px block">
+                                <div className="absolute inline-flex items-center rounded-r-md border border-gray-300 bg-gray-50 px-2 py-1.5 text-xs font-medium text-blue-500 ">
+                                    unresolved
+                                </div>
+                            </div> */}
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                // placeholder="you@example.com"
+                            />
+                            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                                <QuestionMarkCircleIcon
+                                    className="h-5 w-5 text-gray-400"
+                                    aria-hidden="true"
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <Table />
